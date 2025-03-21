c5dae290c256c09ac0a1fb80e8a0bf40c27a9612
Saif <saif.shabir12@gmail.com>
2025-03-21 15:30:31 +0500

    feat/database connection

---
+++ lib/db.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const mongoString = process.env.MONGODB_URL!;
+
+if (!mongoString) throw new Error("Please check Mongo Url");
+
+let cached = globalThis.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectToDatabase() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: true,
+      maxPoolSize: 10,
+    };
+
+    cached.promise = mongoose
+      .connect(mongoString, opts)
+      .then(() => mongoose.connection);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    console.error(error);
+    throw new Error("Check Database file");
+  }
+  return cached.conn;
+}
